22d87fdd3e72d8315fe5b0622886b365e56ba3de
nisitin <seiya110307@icloud.com>
2022-12-06 22:09:12 +0900

    fix: 修正

--- app/Head.tsx
+++ app/Head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Next.js 13</title>
+      <title>Fukuokaぐるめ</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
     </>
   );
