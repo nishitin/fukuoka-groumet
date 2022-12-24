5f6787beece19ed9b88c326f336f092919666461
nisitin <seiya110307@icloud.com>
2022-12-24 21:43:28 +0900

    feat: 値を追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <main className="text-red-500">Hello Next.js 13</main>;
+import { SearchForm } from '../ui/SearchForm';
+import { StoreCard } from '../ui/StoreCard';
+import { SubmitForm } from '../ui/SubmitForm.client';
+
+async function fetchFukuokaGourmetData() {
+  const API_KEY = process.env.API_KEY as string;
+  const res = await fetch(API_KEY);
+
+  const data = await res.json();
+
+  return data;
+}
+
+export default async function Home() {
+  const data = await fetchFukuokaGourmetData();
+
+  return (
+    <div className="space-y-12">
+      <div className="flex justify-center w-full">
+        <SearchForm />
+        <SubmitForm />
+      </div>
+      <div className="flex justify-center w-full">
+        <StoreCard data={data} />
+      </div>
+    </div>
+  );
 }
